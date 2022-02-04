b5427d229bb3fb5fe5e2c36b239e4dd004135ee2
jloboguapo <jloboguapo@gmail.com>
2022-02-03 19:38:46 -0700

    DOM-Cardio exercises

--- exercises/20 - The DOM/DOM-Cardio.js
+++ exercises/20 - The DOM/DOM-Cardio.js
@@ -1,44 +1,79 @@
-// Make a div
+const div = document.createElement('div');
 
-// add a class of wrapper to it
+div.classList.add('wrapper');
 
-// put it into the body
+document.body.appendChild(div);
 
-// make an unordered list
+const ul = `<ul>
+    <li>one</li>
+    <li>two</li>
+    <li>three</li>
+</ul>`
 
-// add three list items with the words "one, two, three" in them
-// put that list into the above wrapper
+div.innerHTML = ul;
 
-// create an image
+const img = document.createElement('img');
 
-// set the source to an image
-// set the width to 250
-// add a class of cute
-// add an alt of Cute Puppy
-// Append that image to the wrapper
+img.src = 'https://picsum.photos/500';
 
-// with HTML string, make a div, with two paragraphs inside of it
-// put this div before the unordered list from above
+img.width = 250;
+img.height = 250;
 
-// add a class to the second paragraph called warning
-// remove the first paragraph
+img.classList.add('cute');
 
-// create a function called generatePlayerCard that takes in three arguments: name, age, and height
+img.alt = 'Cute Puppy!';
 
-// have that function return html that looks like this:
-// <div class="playerCard">
-//   <h2>NAME — AGE</h2>
-//   <p>They are HEIGHT and AGE years old. In Dog years this person would be AGEINDOGYEARS. That would be a tall dog!</p>
-// </div>
+div.appendChild(img);
 
-// make a new div with a class of cards
+const myHTML = `
+    <div>
+        <p>Paragraph One</p>
+        <p>Paragraph Two</p>
+    </div>
+`
+const ulElement = div.querySelector('ul');
 
-// make 4 player cards using generatePlayerCard
+ulElement.insertAdjacentHTML('beforebegin', myHTML);
 
-// append those cards to the div
-// put the div into the DOM just before the wrapper element
-// Bonus, put a delete Button on each card so when you click it, the whole card is removed
+const myDiv = div.querySelector('div');
+myDiv.classList.add('myDiv');
 
-// select all the buttons!
-// make out delete function
-// loop over them and attach a listener
+myDiv.lastElementChild.classList.add('warning');
+
+myDiv.firstElementChild.remove();
+
+const cards = document.createElement('div');
+cards.classList.add('cards');
+
+
+function generatePlayerCard(name, age, height) {
+    const html = `
+    <div class='playerCard'>
+    <h2>${name} - ${age}</h2>
+    <p>Their height is ${height} and they are ${age} years old.  In dog  years this person would be ${age * 7}.  That would be a tall dog!
+    </p>
+    <button class='delete' type='button'>&times; Delete</button>
+    </div>
+    `;
+
+    return html;
+    
+};
+
+let cardsHTML = generatePlayerCard('Jon', 22, 70);
+cardsHTML += generatePlayerCard('Ron', 32, 78);
+cardsHTML += generatePlayerCard('Jan', 26, 72);
+cardsHTML += generatePlayerCard('Ran', 13, 59);
+
+cards.innerHTML = cardsHTML;
+
+div.insertAdjacentElement('beforebegin', cards);
+
+const buttons = cards.querySelectorAll('.delete');
+
+function deleteCard(event) {
+    const buttonThatGotClicked = event.currentTarget;
+    buttonThatGotClicked.closest('.playerCard').remove();
+}
+
+buttons.forEach(button => button.addEventListener('click', deleteCard));
