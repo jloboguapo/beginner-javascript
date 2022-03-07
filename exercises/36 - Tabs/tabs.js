f0d66d86499bafc63cbdb7daabc294878c61a115
jloboguapo <jloboguapo@gmail.com>
2022-03-07 14:31:24 -0700

    do tabs exercises

--- exercises/36 - Tabs/tabs.js
+++ exercises/36 - Tabs/tabs.js
@@ -1,1 +1,23 @@
-console.log('ya ya wes we get it.. IT WORKS!');
+const tabs = document.querySelector('.tabs');
+const tabButtons = tabs.querySelectorAll('[role="tab');
+const tabPanels = Array.from(tabs.querySelectorAll('[role="tabpanel'));
+
+function handleTabClick(e) {
+  tabPanels.forEach(panel => {
+    panel.hidden = true;
+  });
+
+  tabButtons.forEach(tab => {
+    tab.setAttribute('aria-selected', false);
+  });
+
+  e.currentTarget.setAttribute('aria-selected', true);
+
+  const { id } = e.currentTarget;
+  const tabPanel = tabPanels.find(
+    panel => panel.getAttribute('aria-labelledby') === id
+  );
+  tabPanel.hidden = false;
+}
+
+tabButtons.forEach(button => button.addEventListener('click', handleTabClick));
