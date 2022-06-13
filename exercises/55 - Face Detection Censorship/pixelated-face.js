56ae5b5360d5c8823e0c621c0a40aded50a3a485
jloboguapo <jloboguapo@gmail.com>
2022-06-13 14:49:21 -0700

    face detection and censorship exercise

--- exercises/55 - Face Detection Censorship/pixelated-face.js
+++ exercises/55 - Face Detection Censorship/pixelated-face.js
@@ -1,3 +1,87 @@
 // The face detection does not work on all browsers and operating systems.
 // If you are getting a `Face detection service unavailable` error or similar,
-// it's possible that it won't work for you at the moment.
+// it's possible that it won't work for you at the moment
+
+const video = document.querySelector('.webcam');
+const canvas = document.querySelector('.video');
+const ctx = canvas.getContext('2d');
+const faceCanvas = document.querySelector('.face');
+const faceCtx = faceCanvas.getContext('2d');
+const faceDetector = new window.FaceDetector();
+const optionsInputs = document.querySelectorAll(
+  '.controls input[type="range"]'
+);
+
+const options = {
+  SIZE: 10,
+  SCALE: 1.35,
+};
+
+function handleOption(e) {
+  const { value, name } = e.currentTarget;
+  options[name] = parseFloat(value);
+}
+
+optionsInputs.forEach(input => input.addEventListener('input', handleOption));
+
+async function populateVideo() {
+  const stream = await navigator.mediaDevices.getUserMedia({
+    video: { width: 1280, height: 720 },
+  });
+  video.srcObject = stream;
+  await video.play();
+  canvas.width = video.videoWidth;
+  canvas.height = video.videoHeight;
+  faceCanvas.width = video.videoWidth;
+  faceCanvas.height = video.videoHeight;
+}
+
+function drawFace(face) {
+  const { width, height, top, left } = face.boundingBox;
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  ctx.strokeStyle = '#ffc600';
+  ctx.lineWidth = 2;
+  ctx.strokeRect(left, top, width, height);
+}
+
+function censor({ boundingBox: face }) {
+  faceCtx.imageSmoothingEnabled = false;
+  faceCtx.clearRect(0, 0, faceCanvas.width, faceCanvas.height);
+  faceCtx.drawImage(
+    video,
+    face.x,
+    face.y,
+    face.width,
+    face.height,
+    face.x,
+    face.y,
+    options.SIZE,
+    options.SIZE
+  );
+
+  const width = face.width * options.SCALE;
+  const height = face.height * options.SCALE;
+  faceCtx.drawImage(
+    faceCanvas,
+    face.x,
+    face.y,
+    options.SIZE,
+    options.SIZE,
+    face.x - (width - face.width) / 2,
+    face.y - (height - face.height) / 2,
+    width,
+    height
+  );
+}
+
+async function detect() {
+  const faces = await faceDetector.detect(video);
+  faces.forEach(drawFace);
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  faces.forEach(censor);
+  requestAnimationFrame(detect);
+  faces.length === 0 &&
+    faceCtx.clearRect(0, 0, faceCanvas.width, faceCanvas.height);
+}
+
+populateVideo().then(detect);
